1aa17c169c6b5ebc486af173320189607521bfd0
DaCodia <Datcoder.hypothermic@gmail.com>
2023-11-18 00:42:20 -0600

    sadge

--- src/main/java/org/valkyrienskies/malumian_skies/registry/item/MSItemRegistry.java
+++ src/main/java/org/valkyrienskies/malumian_skies/registry/item/MSItemRegistry.java
@@ -18,8 +18,8 @@ import static org.valkyrienskies.malumian_skies.MalumianSkies.REGISTRATE;
 public class MSItemRegistry extends Items {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
 
-    public static final RegistryEntry<Item> HOLLOW_ALLOY = REGISTRATE.item("Hollow Alloy", Item::new).register();
-    public static final RegistryEntry<Item> VOLATILE_POWDER = REGISTRATE.item("Lead Ingot", Item::new).register();
+    public static final RegistryEntry<Item> HOLLOW_ALLOY = REGISTRATE.item("hollow_alloy", Item::new).register();
+    public static final RegistryEntry<Item> VOLATILE_POWDER = REGISTRATE.item("lead_ingot", Item::new).register();
     public static final RegistryEntry<Item> ARCANE_LENSE = REGISTRATE.item("Arcane Lense", Item::new).register();
     public static final RegistryObject<SpiritShardItem> GRAVITATIONAL_SPIRIT = ITEMS.register("d", () -> new SpiritShardItem(DEFAULT_PROPERTIES(), MalumianSkiesSpiritTypeRegistry.GRAVITATIONAL_SPIRIT));
     public static void register() {
